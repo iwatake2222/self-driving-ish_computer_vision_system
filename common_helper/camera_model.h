5513a2a0089ad42b7b0836c1e4a63fc66d855d9f
iwatake2222 <take.iwiw2222@gmail.com>
2021-09-07 16:44:47 +0900

    Update cv module

--- common_helper/camera_model.h
+++ common_helper/camera_model.h
@@ -31,6 +31,9 @@ limitations under the License.
 #define M_PI 3.141592653f
 #endif
 
+static inline float Deg2Rad(float deg) { return static_cast<float>(deg * M_PI / 180.0); }
+static inline float Rad2Deg(float rad) { return static_cast<float>(rad * 180.0 / M_PI); }
+
 class CameraModel {
 public:
     class Parameter {
@@ -54,6 +57,7 @@ public:
 
         /* float, 3 x 3 */
         cv::Mat K;
+        cv::Mat K_new;
 
         /* float, 5 x 1 */
         cv::Mat dist_coeff;
@@ -64,9 +68,9 @@ public:
         /* Default Parameters */
         Parameter() {
             SetIntrinsic(1280, 720, 500.0f);
+            SetDist({ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f });
+            //SetDist({ -0.1f, 0.01f, -0.005f, -0.001f, 0.0f });
             SetExtrinsic({ 0, 0, 0 }, { 0, 0, 0 });
-            dist_coeff = (cv::Mat_<float>(5, 1) << 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
-            //dist_coeff = (cv::Mat_<float>(5, 1) << -0.1f, 0.01f, -0.005f, -0.001f, 0.0f);
         }
 
         void SetIntrinsic(int32_t _width, int32_t _height, float focal_length) {
@@ -77,11 +81,30 @@ public:
                 focal_length,            0,  width / 2.f,
                            0, focal_length, height / 2.f,
                            0,            0,            1);
+            UpdateNewCameraMatrix();
         }
 
-        void SetExtrinsic(const std::array<float, 3>& r_deg, const std::array<float, 3>& t) {
+        void SetDist(const std::array<float, 5>& dist) {
+            dist_coeff = (cv::Mat_<float>(5, 1) << dist[0], dist[1], dist[2], dist[3], dist[4]);
+            UpdateNewCameraMatrix();
+        }
+
+        void UpdateNewCameraMatrix()
+        {
+            if (!K.empty() && !dist_coeff.empty()) {
+                K_new = cv::getOptimalNewCameraMatrix(K, dist_coeff, cv::Size(width, height), 0.0);
+            }
+        }
+
+        void SetExtrinsic(const std::array<float, 3>& r_deg, const std::array<float, 3>& t, bool is_t_on_world = false) {
             rvec = (cv::Mat_<float>(3, 1) << Deg2Rad(r_deg[0]), Deg2Rad(r_deg[1]), Deg2Rad(r_deg[2]));
             tvec = (cv::Mat_<float>(3, 1) << t[0], t[1], t[2]);
+
+            if (is_t_on_world) {
+                auto R = MakeRotateMat(r_deg[0], r_deg[1], r_deg[2]);
+                tvec = R * tvec;
+            }
+
         }
 
         void GetExtrinsic(std::array<float, 3>& r_deg, std::array<float, 3>& t) {
@@ -92,35 +115,41 @@ public:
 
     Parameter parameter;
 
-    static inline float Deg2Rad(float deg) { return static_cast<float>(deg * M_PI / 180.0); }
-    static inline float Rad2Deg(float rad) { return static_cast<float>(rad * 180.0 / M_PI); }
-
     static float FocalLength(int32_t image_size, float fov)
     {
         /* (w/2) / f = tan(fov/2) */
         return (image_size / 2) / std::tanf(Deg2Rad(fov / 2));
     }
 
-    static cv::Mat MakeRotateMat(float x_deg, float y_deg, float z_deg)
+    template <typename T = float>
+    static cv::Mat MakeRotateMat(T x_deg, T y_deg, T z_deg)
     {
-        float x_rad = Deg2Rad(x_deg);
-        float y_rad = Deg2Rad(y_deg);
-        float z_rad = Deg2Rad(z_deg);
-        cv::Mat R_x = (cv::Mat_<float>(3, 3) <<
+
+        T x_rad = Deg2Rad(x_deg);
+        T y_rad = Deg2Rad(y_deg);
+        T z_rad = Deg2Rad(z_deg);
+#if 0
+        cv::Mat R_x = (cv::Mat_<T>(3, 3) <<
             1, 0, 0,
             0, std::cos(x_rad), -std::sin(x_rad),
             0, std::sin(x_rad), std::cos(x_rad));
 
-        cv::Mat R_y = (cv::Mat_<float>(3, 3) <<
+        cv::Mat R_y = (cv::Mat_<T>(3, 3) <<
             std::cos(y_rad), 0, std::sin(y_rad),
             0, 1, 0,
             -std::sin(y_rad), 0, std::cos(y_rad));
 
-        cv::Mat R_z = (cv::Mat_<float>(3, 3) <<
+        cv::Mat R_z = (cv::Mat_<T>(3, 3) <<
             std::cos(z_rad), -std::sin(z_rad), 0,
             std::sin(z_rad), std::cos(z_rad), 0,
             0, 0, 1);
-        cv::Mat R = R_z * R_y * R_x;
+        
+        cv::Mat R = R_z * R_x * R_y;
+#else
+        cv::Mat r = (cv::Mat_<T>(3, 1) << x_rad, y_rad, z_rad);
+        cv::Mat R;
+        cv::Rodrigues(r, R);
+#endif
         return R;
     }
 
@@ -182,10 +211,11 @@ public:
         image_point.y = vv * parameter.fy() + parameter.cy();
     }
 
-    void PRINT_MAT_FLOAT(const cv::Mat& mat, int32_t size)
+    template <typename T = float>
+    static void PRINT_MAT_FLOAT(const cv::Mat& mat, int32_t size)
     {
         for (int32_t i = 0; i < size; i++) {
-            printf("%d: %.3f\n", i, mat.at<float>(i));
+            printf("%d: %.3f\n", i, mat.at<T>(i));
         }
     }
 
@@ -194,7 +224,7 @@ public:
         /*** Undistort image point ***/
         std::vector<cv::Point2f> original_uv{ image_point };
         std::vector<cv::Point2f> image_point_undistort;
-        cv::undistortPoints(original_uv, image_point_undistort, parameter.K, parameter.dist_coeff, parameter.K);
+        cv::undistortPoints(original_uv, image_point_undistort, parameter.K, parameter.dist_coeff, parameter.K);    /* don't use K_new */
         float u = image_point_undistort[0].x;
         float v = image_point_undistort[0].y;
 
@@ -213,6 +243,7 @@ public:
 
         cv::Mat K = parameter.K;
         cv::Mat R = MakeRotateMat(Rad2Deg(parameter.pitch()), Rad2Deg(parameter.yaw()), Rad2Deg(parameter.roll()));
+
         cv::Mat K_inv;
         cv::invert(K, K_inv);
         cv::Mat R_inv;
@@ -222,8 +253,8 @@ public:
 
         /* calculate s */
         cv::Mat LEFT_WO_S = R_inv * K_inv * UV;
-        cv::Mat RIGHT_WO_M = R_inv * t;
-        float s = (t.at<float>(1) + RIGHT_WO_M.at<float>(1)) / LEFT_WO_S.at<float>(1);
+        cv::Mat RIGHT_WO_M = R_inv * t;         /* no need to add M because M[1] = 0 (ground plane)*/
+        float s = RIGHT_WO_M.at<float>(1) / LEFT_WO_S.at<float>(1);
 
         /* calculate M */
         cv::Mat TEMP = R_inv * (s * K_inv * UV - t);
@@ -231,6 +262,7 @@ public:
         object_point.x = TEMP.at<float>(0);
         object_point.y = TEMP.at<float>(1);
         object_point.z = TEMP.at<float>(2);
+        if (object_point.z < 0) object_point.z = 999;
 
         //PRINT_MAT_FLOAT(TEMP, 3);
     }
@@ -250,8 +282,12 @@ public:
 
     int32_t EstimateVanishmentY()
     {
-        float px_from_center = std::tan(parameter.pitch()) * parameter.fy();
-        float vanishment_y = parameter.cy() - px_from_center;
+        float fy = parameter.fy();
+        float cy = parameter.cy();
+        //float fy = parameter.K_new.at<float>(4);
+        //float cy = parameter.K_new.at<float>(5);
+        float px_from_center = std::tan(parameter.pitch()) * fy;
+        float vanishment_y = cy - px_from_center;
         return static_cast<int32_t>(vanishment_y);
     }
 
---
+++ common_helper/curve_fitting.h
@@ -0,0 +1,111 @@
+/* Copyright 2021 iwatake2222
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef CURVE_FITTING_
+#define CURVE_FITTING_
+
+/*** Include ***/
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <string>
+#include <vector>
+#include <array>
+
+#include <opencv2/opencv.hpp>
+
+class CurveFitting
+{
+public:
+    /* y = ax + b */
+    /* point = (x, y) */
+    template <typename T = int32_t>
+    static bool SolveLinearRegression(const std::vector<cv::Point_<T>>& point_list, double& a, double& b)
+    {
+        if (point_list.size() < 2) return false;
+        double x_avg = 0;
+        double y_avg = 0;
+        for (const auto& p : point_list) {
+            x_avg += p.x;
+            y_avg += p.y;
+        }
+        x_avg /= point_list.size();
+        y_avg /= point_list.size();
+
+        double s_xy = 0;
+        double s_xx = 0;
+        for (const auto& p : point_list) {
+            s_xy += (p.x - x_avg) * (p.y - y_avg);
+            s_xx += (p.x - x_avg) * (p.x - x_avg);
+        }
+
+        a = s_xy / s_xx;
+        b = y_avg - a * x_avg;
+        return true;
+    }
+
+    /* y = ax^2 + bx + c */
+    /* point = (x, y) */
+    template <typename T>
+    static bool SolveQuadraticRegression(const std::vector<cv::Point_<T>>& point_list, double& a, double& b, double& c)
+    {
+        if (point_list.size() < 3) return false;
+        /*
+        | sum(x^4) sum(x^3) sum(x^2) |   | a |   | sum(x*x*y) |
+        | sum(x^3) sum(x^2) sum(x^1) | * | b | = | sum(x*y)   |
+        | sum(x^2) sum(x^1) sum(x^0) |   | c |   | sum(y)     |
+        */
+        double sum_x4 = 0;
+        double sum_x3 = 0;
+        double sum_x2 = 0;
+        double sum_x1 = 0;
+        double sum_x0 = point_list.size();
+        double sum_xxy = 0;
+        double sum_xy = 0;
+        double sum_y = 0;
+        for (const auto& p : point_list) {
+            sum_y += p.y;
+            double xy = p.x * p.y;
+            sum_xy += xy;
+            sum_xxy += p.x * xy;
+
+            double x1 = p.x;
+            double x2 = x1 * x1;
+            double x3 = x1 * x2;
+            double x4 = x2 * x2;
+            sum_x1 += x1;
+            sum_x2 += x2;
+            sum_x3 += x3;
+            sum_x4 += x4;
+        }
+
+        cv::Mat LEFT = (cv::Mat_<double>(3, 3) << sum_x4, sum_x3, sum_x2, sum_x3, sum_x2, sum_x1, sum_x2, sum_x1, sum_x0);
+        cv::Mat RIGHT = (cv::Mat_<double>(3, 1) << sum_xxy, sum_xy, sum_y);
+
+        cv::Mat LEFT_inv;
+        if (cv::invert(LEFT, LEFT_inv) == 0) return false;
+
+        cv::Mat mat = LEFT_inv * RIGHT;
+        a = mat.at<double>(0);
+        b = mat.at<double>(1);
+        c = mat.at<double>(2);
+
+        return true;
+    }
+};
+
+
+#endif
